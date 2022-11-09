8d090c04e2ffdf51dcfdc3d95bb64fceb2f15974
furkanGit <furkancaliskan9801@gmail.com>
2022-11-09 17:13:00 +0100

    Js Done

--- js/main.js
+++ js/main.js
@@ -6,3 +6,24 @@
 
 
 
+const container = document.getElementById("container");
+
+for (let index = 1; index <= 100; index++) {
+
+    if (index % 3 == 0 && index % 5 == 0) {
+        console.log('FizzBuzz');
+        container.innerHTML += '<div class="box Fizzbuzz">FizzBuzz</div>';
+
+    }else if (index % 5 == 0) {
+        container.innerHTML += '<div class="box Buzz">Buzz</div>';
+        console.log('Buzz');
+    
+    }else if (index % 3 == 0) {
+        container.innerHTML += '<div class="box Fizz">Fizz</div>';
+        console.log('Fizz');
+
+    } else {
+        container.innerHTML += '<div class="box">' + index + '</div>';
+        console.log(index);
+    }
+}
