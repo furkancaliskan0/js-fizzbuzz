b51553484741b76903e529e11a7418f6f39d1831
furkanGit <furkancaliskan9801@gmail.com>
2022-11-09 22:51:59 +0100

    Upgrade Page

--- js/main.js
+++ js/main.js
@@ -11,16 +11,16 @@ const container = document.getElementById("container");
 for (let index = 1; index <= 100; index++) {
 
     if (index % 3 == 0 && index % 5 == 0) {
-        console.log('FizzBuzz');
-        container.innerHTML += '<div class="box Fizzbuzz">FizzBuzz</div>';
+        console.log('fizzbuzz');
+        container.innerHTML += '<div class="box fizzbuzz">fizzbuzz</div>';
 
     }else if (index % 5 == 0) {
-        container.innerHTML += '<div class="box Buzz">Buzz</div>';
-        console.log('Buzz');
+        container.innerHTML += '<div class="box buzz">buzz</div>';
+        console.log('buzz');
     
     }else if (index % 3 == 0) {
-        container.innerHTML += '<div class="box Fizz">Fizz</div>';
-        console.log('Fizz');
+        container.innerHTML += '<div class="box fizz">fizz</div>';
+        console.log('fizz');
 
     } else {
         container.innerHTML += '<div class="box">' + index + '</div>';
