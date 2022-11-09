7a01be22704574bcd8b8a8e506af97cdf1a00c2c
furkanGit <furkancaliskan9801@gmail.com>
2022-11-09 17:52:46 +0100

    Page Completed

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // stampare numeri da 1 a 100
-//i multipli di 3 stampi “Fizz”
-//i multipli di 5 stampi “Buzz”
-//i multipli di 3 che di 5 stampi “FizzBuzz”
+//i multipli di 3 stampi Fizz
+//i multipli di 5 stampi Buzz
+//i multipli di 3 che di 5 stampi FizzBuzz
 
 
 
