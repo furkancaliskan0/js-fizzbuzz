31d90f0796c22c236c0566183ad55c5dd2812200
furkanGit <furkancaliskan9801@gmail.com>
2022-11-10 09:24:36 +0100

    Comments Upgrade

--- js/main.js
+++ js/main.js
@@ -1,18 +1,19 @@
 // stampare numeri da 1 a 100
-//i multipli di 3 stampi Fizz
-//i multipli di 5 stampi Buzz
+//i multipli di 3 stampi fizz
+//i multipli di 5 stampi buzz
 //i multipli di 3 che di 5 stampi FizzBuzz
-
 
 
 
 const container = document.getElementById("container");
 
+
 for (let index = 1; index <= 100; index++) {
+// AND && -Entrambi delle condizioni devono essere vere
 
     if (index % 3 == 0 && index % 5 == 0) {
-        console.log('fizzbuzz');
         container.innerHTML += '<div class="box fizzbuzz">fizzbuzz</div>';
+        console.log('fizzbuzz');
 
     }else if (index % 5 == 0) {
         container.innerHTML += '<div class="box buzz">buzz</div>';
